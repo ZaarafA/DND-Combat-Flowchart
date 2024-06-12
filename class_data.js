ee711cbdb0a550a792a2b411448447401b2c342f
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-06-12 02:04:41 -0400

    Update class_data.js

--- class_data.js
+++ class_data.js
@@ -1,39 +1,47 @@
 const dndClasses = {
-    Barbarian: {
-      1: ["Rage", "1BA"],
-    },
-    Bard: {
-      1: ["Bardic Inspiration", "1BA", "d6"],
-      5: ["Bardic Inspiration", "1BA", "d8"],
-      6: ["Countercharm", "1A"],
-      10: ["Bardic Inspiration", "1BA", "d10"],
-      15: ["Bardic Inspiration", "1BA", "d12"],
-    },
-    Cleric: {
-      1: ["Spellcasting", "Divine Domain"],
-      2: ["Channel Divinity", "1A"],
-      10: ["Divine Intervention", "1A"],
-    },
-    Fighter: {
-        1: ["Second Wind", "1BA"],
-    },
-    Paladin: {
-        1: ["Lay on Hands", "1A"],
-        2: ["Divine Smite", "1A"],
-    },
-    Ranger: {
-        3: ["Primeval Awareness", "1A"],
-        14: ["Vanish", "1BA"],
-    },
-    Rogue: {
-        2: ["Cunning Action", "1BA"],
-        5: ["Uncanny Dodge", "1R"],
-    },
-    Sorcerer: {
-    },
-    Warlock: {
-    },
-    Wizard: {
-    },
+  Barbarian: {
+    1: ["Rage", "1BA"],
+  },
+  Bard: {
+    1: ["Bardic Inspiration", "1BA", "d6"],
+    5: ["Bardic Inspiration", "1BA", "d8"],
+    6: ["Countercharm", "1A"],
+    10: ["Bardic Inspiration", "1BA", "d10"],
+    15: ["Bardic Inspiration", "1BA", "d12"],
+  },
+  Cleric: {
+    1: ["Spellcasting", "Divine Domain"],
+    2: ["Channel Divinity", "1A"],
+    10: ["Divine Intervention", "1A"],
+  },
+  Fighter: {
+    1: ["Second Wind", "1BA"],
+  },
+  Paladin: {
+    1: ["Lay on Hands", "1A"],
+    2: ["Divine Smite", "1A"],
+  },
+  Ranger: {
+    3: ["Primeval Awareness", "1A"],
+    14: ["Vanish", "1BA"],
+  },
+  Rogue: {
+    2: ["Cunning Action", "1BA"],
+    5: ["Uncanny Dodge", "1R"],
+  },
+  Sorcerer: {
+  },
+  Warlock: {
+  },
+  Wizard: {
+  },
+  BloodHunter: {
+    2: ["Crimson Rite", "1BA"], 
+  },
+  Monk: {
+    3: ["Deflect Missiles", "1R"],
+    4: ["Slow Fall", "1R"],
+    7: ["Stillness of Mind", "1A"],
+    18: ["Empty Body", "1A"],
+  },
 };
-  
